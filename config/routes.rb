23ac47a061ae79aa0d1ab42fa290f44f719746c4
trupovar13 <trupovar13@gmail.com>
2017-08-31 08:44:03 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get '/' => 'home#index'
+
+  resources :articles
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
