47892b1a592333ef72ba6c55a965f37ca4d6c1bb
trupovar13 <trupovar13@gmail.com>
2017-09-21 08:45:24 +0300

    Update article spec to include validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,5 +2,14 @@ require "rails_helper"
 #require "spec_helper"
 
 describe Article do
-  it { should have_many :comments } 
+  
+  describe "validations" do
+    it { should validate_presence_of :title } 
+    it { should validate_presence_of :text }
+  end
+
+  describe "associations" do
+    it { should have_many :comments } 
+  end
+
 end
