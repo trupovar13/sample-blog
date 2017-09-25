dabcdd7489e9be8a5aefe605b108fac2d257bf59
trupovar13 <trupovar13@gmail.com>
2017-09-25 09:27:30 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -22,4 +22,13 @@ describe Article do
     end
   end
 
+  describe "#last_comment" do 
+    it "returns the last comment" do
+      # создаем статью но в этот раз с комментариями
+      article = create(:article_with_comments)
+
+      # проверка
+      expect(article.last_comment.body).to eq "comment body 3"
+    end
+  end
 end
