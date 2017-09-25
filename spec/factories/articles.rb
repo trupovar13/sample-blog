eb10beac2be893feef141dc0e2b1f5ea27202972
trupovar13 <trupovar13@gmail.com>
2017-09-25 09:27:12 +0300

    Update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
   factory :article do
     title "Article title"
-    text "Article text" 
+    text "Article text"
+    # создаем фабрику с названием article_with_comments
+    # для создания статьи с несколькими комментариями
+    factory :article_with_comments do 
+      # после создания article
+      after :create do |article, evaluator|
+        # создаем список из 2-ех комментариев
+        create_list :comment, 3, article: article 
+      end
+    end
   end 
 end
